638ebe4fca38d659c7366194b5528c1760e59514
Luka <lukhanyo95@gmail.com>
2023-03-29 08:37:41 +0200

    format code

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
-  }
+    added: new Date(),
+  },
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Mini Message Board', messages: messages});
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
-router.post('/newMessage', function(req, res, next) {
+router.post("/newMessage", function (req, res, next) {
   messages.push({
     text: req.body.message,
     user: req.body.user,
-    added: new Date()
-  })
-  res.redirect('/')
+    added: new Date(),
+  });
+  res.redirect("/");
 });
-
-
 
 module.exports = router;
